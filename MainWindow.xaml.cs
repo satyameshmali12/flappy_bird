47a045b1464b490812dc67764c63755c3f75c781
= <satyameshmaliaass@gmail.com>
2022-09-03 12:57:25 +0530

    ini

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -78,64 +78,129 @@ namespace WpfApp1
     {
 
         public int noofpipe = 10;
+        public double lastxco;
+
+        public int pipewidth = 60;
+        Random rand  = new Random();
 
         // creating the player
         Bird player;
 
-        BitmapImage downpipe;
-        TransformedBitmap uppipe;
+        Rectangle downpiperect;
+        Rectangle uppiperect;
+
 
         public MainWindow()
         {
+            // xco = Width;
             DispatcherTimer dispatcher = new DispatcherTimer();
             dispatcher.Tick += gameloop;
             dispatcher.Interval = TimeSpan.FromMilliseconds(10);
             dispatcher.Start();
             string baseurl1 = "E:\\customs\\csharpdev\\Flappy_BIrd\\WpfApp1\\img";
             InitializeComponent();
-            // creating the background of the game window
-            ImageBrush ground = new ImageBrush();
-            ImageBrush back = new ImageBrush();
-            ground.ImageSource = new BitmapImage(new Uri($"{baseurl1}\\ground.png"));
-            back.ImageSource = new BitmapImage(new Uri($"{baseurl1}\\bg.png"));
-            Rectangle groundrect = new Rectangle(){Width=Width,Height=100,Fill=ground};
-            Canvas.SetLeft(groundrect,0);
-            Canvas.SetBottom(groundrect,0);
-            Rectangle backrect = new Rectangle(){Width=Width,Height=Height,Fill=back};
-            mycan.Children.Add(backrect);
-            mycan.Children.Add(groundrect);  
+
+            // creating the background window
+            addtoscrren($"{baseurl1}\\bg.png",Width,Height,0,-1,-1,0,"background");
+            addtoscrren($"{baseurl1}\\ground.png",Width,100,0,-1,0,-1,"ground");
             player = new Bird(mycan);
 
-
+            // creating the down and up pipe             :- thereby rotating the pipe
             ImageBrush downpipebrush = new ImageBrush();
             ImageBrush uppipebrush = new ImageBrush();
-            downpipe = new BitmapImage(new Uri("E:\\customs\\csharpdev\\Flappy_BIrd\\WpfApp1\\img\\pipe.png"));
+            BitmapImage downpipe = new BitmapImage(new Uri("E:\\customs\\csharpdev\\Flappy_BIrd\\WpfApp1\\img\\pipe.png"));
+            downpipebrush.ImageSource = downpipe;
+            downpiperect = new Rectangle(){Width=60,Height=100,Fill=downpipebrush,Tag="pipe"};
             RotateTransform rt = new RotateTransform(180);
-            uppipe = new TransformedBitmap();
+            TransformedBitmap uppipe = new TransformedBitmap();
+            uppipe.BeginInit();
             uppipe.Source =  downpipe;
             uppipe.Transform = rt;
-            
+            uppipebrush.ImageSource = uppipe;
+            uppipe.EndInit();
+            uppiperect = new Rectangle(){Width=60,Height=100,Fill=uppipebrush,Tag="pipe"};
+
+
+            // sqawning the initial pipe
+            Canvas.SetTop(uppiperect,0);
+            Canvas.SetLeft(uppiperect,Width);
+            mycan.Children.Add(uppiperect);
+
+            Canvas.SetBottom(downpiperect,100);
+            Canvas.SetLeft(downpiperect,Width);
+            mycan.Children.Add(downpiperect);
 
         }
 
-        public void addtoscrren(string url,int width,int height,int left,int right,string tag="no tag"){
+        public void addtoscrren(string url,double width,double height,int left,int right,int bottom,int top,string tag="no tag"){
             ImageBrush custom = new ImageBrush();
             custom.ImageSource = new BitmapImage(new Uri(url));
             Rectangle customrect = new Rectangle(){Width=width,Height=height,Fill=custom,Tag=tag};
-            Canvas.SetLeft(customrect,left);
-            Canvas.SetBottom(customrect,right);
+            if (left>-1)Canvas.SetLeft(customrect,left);
+            if (right>-1)Canvas.SetRight(customrect,right);
+            if (top>-1)Canvas.SetTop(customrect,top);
+            if (bottom>-1)Canvas.SetBottom(customrect,bottom);
+
             mycan.Children.Add(customrect);
+        }
+
+        public void spawnpipe()
+        {
+            ImageBrush downpipebrush = new ImageBrush();
+            BitmapImage downpipe = new BitmapImage(new Uri("E:\\customs\\csharpdev\\Flappy_BIrd\\WpfApp1\\img\\pipe.png"));
+            downpipebrush.ImageSource = downpipe;
+
+            ImageBrush uppipebrush = new ImageBrush();
+            RotateTransform rt = new RotateTransform(180);
+            TransformedBitmap uppipe = new TransformedBitmap();
+            uppipe.BeginInit();
+            uppipe.Source =  downpipe;
+            uppipe.Transform = rt;
+            uppipebrush.ImageSource = uppipe;
+            uppipe.EndInit();
+
+
+            downpiperect = new Rectangle(){Width=60,Height=100,Fill=downpipebrush,Tag="pipe"};
+            uppiperect = new Rectangle(){Width=60,Height=100,Fill=uppipebrush,Tag="pipe"};
+
+            double x = 0;
+
+            foreach (var item in mycan.Children.OfType<Rectangle>()){
+                x = Canvas.GetLeft(item);
+            }
+
+            int rdis = rand.Next(100,200);
+            Canvas.SetTop(uppiperect,0);
+            Canvas.SetLeft(uppiperect,x+pipewidth+rdis);
+            mycan.Children.Add(uppiperect);
+
+            Canvas.SetBottom(downpiperect,100);
+            Canvas.SetLeft(downpiperect,x+pipewidth+rdis);
+            mycan.Children.Add(downpiperect);
+
+
         }
 
         private void gameloop(object sender,EventArgs e)
         {
-            if (noofpipe>10)
-            {
-                
-
+            if (noofpipe!=0)
+            {   
+                spawnpipe();
+                noofpipe-=1;
             }
-            foreach(var item in mycan.Children.OfType<Rectangle>()){
-                
+            foreach(Rectangle item in mycan.Children.OfType<Rectangle>()){
+                if (Convert.ToString(item.Tag) == "pipe"){
+                    Canvas.SetLeft(item,Canvas.GetLeft(item)-3);
+                    lastxco = Canvas.GetLeft(item);
+                    if (lastxco+pipewidth<0){
+                        // mycan.Children.Remove(item);
+                        // noofpipe+=1;
+                        if (mycan.Children.Contains(item)){
+                            mycan.Children.RemoveAt(mycan.Children.IndexOf(item));
+                            noofpipe+=1;
+                        }
+                    }
+                }
             }
             player.fall();
             player.jump();
